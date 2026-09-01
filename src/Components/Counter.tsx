b78d636f369fb08b0fea9a81db4ce11709f227f2
vyombudha <vyombudha7@gmail.com>
2026-09-01 09:41:38 +0545

    add project files

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,42 @@
+import {useState} from 'react'
+  import { toast } from 'react-toastify';
+import Error from './Error';
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function adder() {
+    setCounter(counter + 1);
+  }
+
+  function subtract() {
+    if (counter - 1 < 0) {
+      toast.error('Cannot Get Counter Below Zero', {
+position: "top-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: false,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "dark",
+});
+      return; 
+    }
+    setCounter(counter - 1);
+
+  }
+  return (
+    <div>
+      <h1>Counter =  {counter}</h1>
+      <h3>
+        <button onClick={adder}>add</button>
+        <button onClick={subtract}>sub</button>
+      </h3>
+      <Error />
+    </div>
+    
+  )
+}
+
+export default Counter
---
+++ src/Components/Error.tsx
@@ -0,0 +1,19 @@
+  import { ToastContainer } from 'react-toastify';
+function Error() {
+  return (
+    <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick={false}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="dark"
+/>
+  )
+}
+
+export default Error
